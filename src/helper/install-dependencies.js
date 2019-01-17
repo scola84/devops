fc7938d98b80b1f403acbafb577ff9c5788a82ac
M.C. van den Boogaart <scola@kvdb.name>
2019-01-17 15:13:27 +0100

    Make install more sync

--- src/helper/install-dependencies.js
+++ src/helper/install-dependencies.js
@@ -1,6 +1,5 @@
-import each from 'async/each';
-import { exec } from 'child_process';
-import { readFile, writeFile } from 'fs';
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
 import trim from 'lodash-es/trim';
 import recursive from 'recursive-readdir';
 
@@ -8,6 +7,7 @@ const skip = [
   '..',
   '.',
   'child_process',
+  'cluster',
   'crypto',
   'fs',
   'http',
@@ -24,84 +24,56 @@ const skip = [
 export default function installDependencies(callback) {
   recursive(process.cwd() + '/src', (error, files) => {
     if (error) {
-      return callback(error);
+      callback(error);
+      return;
     }
 
-    const modules = new Set();
-    const self = process.cwd().split('/').slice(-2).join('/');
+    try {
+      install(files);
+      callback();
+    } catch (installError) {
+      callback(installError);
+    }
+  });
+}
 
-    return each(files, (file, eachCallback) => {
-      readFile(file, (error2, data) => {
-        if (error) {
-          return eachCallback(error2);
+function install(files) {
+  const modules = new Set();
+  const self = process.cwd().split('/').slice(-2).join('/');
+
+  files.forEach((file) => {
+    const data = readFileSync(file);
+    const match = String(data).match(/^import ([^;]|\n)*;$/mg) || [];
+    let module = null;
+    let scope = null;
+
+    for (let i = 0; i < match.length; i += 1) {
+      module = trim(match[i].slice(match[i].indexOf('\'')), ' \';');
+
+      [scope, module] = module.split('/');
+
+      module = module && scope.slice(0, 1) === '@' ?
+        ([scope, module].join('/')) : scope;
+
+      if (skip.indexOf(module) === -1) {
+        if (module !== self) {
+          modules.add(module);
         }
-
-        const match = String(data).match(/^import ([^;]|\n)*;$/mg) || [];
-        let module = null;
-        let scope = null;
-
-        for (let i = 0; i < match.length; i += 1) {
-          module = trim(match[i].slice(match[i].indexOf('\'')), ' \';');
-
-          [scope, module] = module.split('/');
-
-          module = module && scope.slice(0, 1) === '@' ?
-            ([scope, module].join('/')) : scope;
-
-          if (skip.indexOf(module) === -1) {
-            if (module !== self) {
-              modules.add(module);
-            }
-          }
-        }
-
-        return eachCallback();
-      });
-    }, (errorA) => {
-      if (errorA) {
-        return callback(errorA);
       }
-
-      const options = { cwd: process.cwd() };
-      const names = Array.from(modules).sort().join(' ');
-
-      return readFile(process.cwd() + '/package.json', (error3, data) => {
-        if (error3) {
-          return callback(error3);
-        }
-
-        let json = JSON.parse(String(data));
-        delete json.dependencies;
-
-        json = JSON.stringify(json);
-
-        return writeFile(process.cwd() + '/package.json', json, (error4) => {
-          if (error4) {
-            return callback(error4);
-          }
-
-          return exec('npm install ' + names, options, (error5) => {
-            if (error5) {
-              return callback(error5);
-            }
-
-            return exec('rm -rf node_modules', options, (error6) => {
-              if (error6) {
-                return callback(error6);
-              }
-
-              return exec('rm package-lock.json', options, (error7) => {
-                if (error7) {
-                  return callback(error7);
-                }
-
-                return callback();
-              });
-            });
-          });
-        });
-      });
-    });
+    }
   });
 
+  const data = readFileSync(process.cwd() + '/package.json');
+  const json = JSON.parse(String(data));
+
+  delete json.dependencies;
+
+  writeFileSync(process.cwd() + '/package.json', JSON.stringify(json));
+
+  const options = { cwd: process.cwd(), stdio: 'inherit' };
+  const names = Array.from(modules).sort().join(' ');
+
+  execSync('npm install ' + names, options);
+  execSync('rm -rf node_modules', options);
+  execSync('rm package-lock.json', options);
 }
