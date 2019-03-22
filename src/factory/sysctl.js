b4f4a32cfda82506347724b5d233f74ea581cacd
M.C. van den Boogaart <scola@kvdb.name>
2019-03-22 10:47:34 +0100

    Add sysctl restarter

--- src/factory/sysctl.js
+++ src/factory/sysctl.js
@@ -2,25 +2,12 @@ import { Commander, ctl, sed } from '@scola/ssh';
 import { Worker } from '@scola/worker';
 
 export default function createSysctl({
+  restart = false,
   update = false,
   execute = null
 }) {
   const beginner = new Worker();
   const ender = new Worker();
-
-  const executer = new Commander({
-    description: 'Execute sysctl',
-    command(box, data) {
-      const commands = [];
-      const items = this.resolve(execute, box, data);
-
-      items.forEach(({ action, name }) => {
-        commands.push(ctl(action, name));
-      });
-
-      return commands;
-    }
-  });
 
   const updater = new Commander({
     description: 'Update sysctl',
@@ -40,8 +27,36 @@ export default function createSysctl({
     }
   });
 
+  const restarter = new Commander({
+    description: 'Restart sysctl',
+    confirm: true,
+    decide(box) {
+      return box.start === true;
+    },
+    command() {
+      return [
+        ctl('daemon-reload')
+      ];
+    }
+  });
+
+  const executer = new Commander({
+    description: 'Execute sysctl',
+    command(box, data) {
+      const commands = [];
+      const items = this.resolve(execute, box, data);
+
+      items.forEach(({ action, name }) => {
+        commands.push(ctl(action, name));
+      });
+
+      return commands;
+    }
+  });
+
   beginner
     .connect(update === true ? updater : null)
+    .connect(restart === true ? restarter : null)
     .connect(execute !== null ? executer : null)
     .connect(ender);
 
